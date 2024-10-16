7938e3427e3adce7f06d4e77b132d7c798668c87
kuuuuumiiiii <kuuuuumiiiii@users.noreply.github.com>
2024-10-17 00:15:51 +0900

    ノートを追加する

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
+import { useState } from 'react';
 import './App.css'
+import Main from './components/Main';
+import Sidebar from './components/Sidebar';
+import uuid from 'react-uuid';
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  const onAddNote = () => {
+    console.log("新しくノートを追加");
+    const newNote = {
+      id: uuid(),
+      title: "新しいノート",
+      content: "新しいノートの内容",
+      modDate: Date.now(),
+    };
+    setNotes([...notes, newNote]);
+    console.log(notes);
+  }
+
   return (
-    <div className='App'></div>
+    <div className='App'>
+      <Sidebar onAddNote= {onAddNote} notes={notes}/>
+      <Main />
+    </div>
   )
 }
 
